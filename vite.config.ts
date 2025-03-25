404df82fadb9c9f04ff667ff6d45a5f6655e4406
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-03-25 19:57:52 +0100

    styling and configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {TanStackRouterVite} from '@tanstack/router-plugin/vite'
+import tsconfigpath from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({target: 'react', autoCodeSplitting: true}),
+    tsconfigpath(),
+    react(),
+  ],
+  define: {
+    'process.env' : {}
+  }
 })
