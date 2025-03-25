a7433b0ca84aa2439705262d0521ebaa9dc87f5e
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-03-25 19:57:22 +0100

    functions to provide any needed providers up to this point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,44 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import {RouterProvider, createRouter} from '@tanstack/react-router'
+import {Provider as ChakraProvider} from '@/components/ui/provider'
+import {routeTree} from './routeTree.gen'
 import './index.css'
-import App from './App.tsx'
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//TODO create a way to read in dotenv in vite
+
+const port: number = Number(process.env.GRAPHQL_PORT) || 4000;
+const path: string = process.env.GRAPHQL_PATH || "/graphql";
+
+const URL = `http://localhost:${port}${path}`
+console.log(URL)
+
+
+const apolloClient = new ApolloClient({uri : URL, cache: new InMemoryCache(), connectToDevTools: true})
+
+const router = createRouter({routeTree, defaultPreload: 'intent'})
+
+declare module '@tanstack/react-router' {
+  interface Register{
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <ApolloProvider client={apolloClient}>
+        
+        <ChakraProvider>
+        <RouterProvider router={router} />
+        <TanStackRouterDevtools router={router}/>
+
+        </ChakraProvider>
+      </ApolloProvider>
+    </StrictMode>,
+  )
+}
