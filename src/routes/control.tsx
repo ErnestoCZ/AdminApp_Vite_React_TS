cd22831794e8ddacea09ea5b5dae6ba57f739a7b
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-04-03 11:08:32 +0200

    added control icons in renderUser for deleting and editing user

--- src/routes/control.tsx
+++ src/routes/control.tsx
@@ -1,55 +1,91 @@
-import { Box, Input, Stack } from '@chakra-ui/react'
-import { createFileRoute } from '@tanstack/react-router'
-import { UserInputForm } from '@/components/UserInputForm'
-import { List } from '@/components/List'
-import {User} from '@/generated/graphql'
-import { fakeUser } from '@/fakeData'
-import { useEffect, useState } from 'react'
-import { useAllUsers } from '@/graphql/hooks'
-import { ListItem } from '@/components/ListItem'
+import { Box, Button, Center, Flex, HStack, Input, Stack } from "@chakra-ui/react";
+import { createFileRoute } from "@tanstack/react-router";
+import { UserInputForm } from "@/components/UserInputForm";
+import { List } from "@/components/List";
+import { User, useUsersQuery } from "@/generated/graphql";
+import { useState } from "react";
+import { RiDeleteBinLine, RiEditLine } from "react-icons/ri";
+// import { useAllUsers } from '@/graphql/hooks'
+import { ListItem } from "@/components/ListItem";
 
-export const Route = createFileRoute('/control')({
+export const Route = createFileRoute("/control")({
   component: RouteComponent,
-})
+});
 
 const renderUser = (item: User) => {
   return (
-    <ListItem>
-    <div key={item.id}>
-      {item.firstName}
-    </div>
+    <ListItem key={item.id}>
+      <Flex justifyContent={"space-between"}>
+        <Stack direction={"row"} key={item.id}>
+          <Box alignContent={"center"}>{item.firstName}</Box>
+          <Box alignContent={"center"}>{item.lastName}</Box>
+        </Stack>
+        <HStack gap={0}>
+          <Button
+            size={"sm"}
+            color={"red"}
+            variant={"ghost"}
+            background={"transparent"}
+          >
+            <RiEditLine />
+          </Button>
+
+          <Button
+            size={"sm"}
+            color={"red"}
+            variant={"ghost"}
+            background={"transparent"}
+          >
+            <RiDeleteBinLine />
+          </Button>
+        </HStack>
+      </Flex>
     </ListItem>
-  )
-}
+  );
+};
 
 function RouteComponent() {
   const [suggestion, setSuggestion] = useState<string>("");
-  const [filteredUser, setFilteredUser] = useState<User[]>([]);
-  const {data, loading, error} = useAllUsers();
 
-
-  useEffect(() => {
-    const newArray = fakeUser.filter(element => element.firstName.toLowerCase().includes(suggestion.toLowerCase()))
-    setFilteredUser(newArray)
-  }, [suggestion])
+  const { data, error } = useUsersQuery({
+    variables: {
+      limit: 20,
+      offset: 0,
+    },
+  });
+  const users =
+    data?.Users.items.filter((element) =>
+      element.lastName?.toLowerCase().includes(suggestion.toLowerCase())
+    ) ?? [];
 
   return (
-
-    <Stack direction={'row'} width={'100%'} height={'100%'}>
-
-      <Stack direction={'column'} flex={'1 1 auto'} padding={2} minWidth={'30%'}>
+    <Stack direction={"row"} width={"100%"} height={"100%"}>
+      <Stack
+        direction={"column"}
+        flex={"1 1 auto"}
+        padding={2}
+        minWidth={"30%"}
+      >
         UserInputForm
-        <UserInputForm/>
+        <UserInputForm />
       </Stack>
 
-      <Stack direction={'column'} flex={'2 1 auto'} padding={2} minWidth={'50%'}>
+      <Stack
+        direction={"column"}
+        flex={"2 1 auto"}
+        padding={2}
+        minWidth={"50%"}
+        maxHeight={"70%"}
+      >
         UserList
-        <Input type='text' value={suggestion} onChange={(e) => setSuggestion(e.target.value)} placeholder='Search'/>
-        <List<User> items={filteredUser} render={renderUser} />
-
+        <Input
+          type="search"
+          value={suggestion}
+          onChange={(e) => setSuggestion(e.target.value)}
+          placeholder="Search"
+        />
+        {error ? <Center> {`${error.message} data`}</Center> :<List<User> items={users} render={renderUser} />}
       </Stack>
-
     </Stack>
-
-  )
+  );
 }
