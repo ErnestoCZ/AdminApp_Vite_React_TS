6b79f93c63b2d64cbf83da72d0f9063d6a5941d1
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-04-03 11:05:12 +0200

    added Flex wrapper for listItems

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,3 +1,4 @@
+import { Flex } from "@chakra-ui/react"
 
 interface ListProps<T> {
     items: T[],
@@ -7,8 +8,8 @@ interface ListProps<T> {
 
 export const List = <T,> ({items,render} : ListProps<T>) => {
   return (
-    <>
+    <Flex direction={'column'} width={'100%'} height={'100%'} gap={2} overflowY={'scroll'} scrollbar={'hidden'}>
     {items.map(element => (render(element)))}
-    </>
+    </Flex>
   )
 }
