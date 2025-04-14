709e7e2c082fded4e4f79839f28cfba127a76bf4
Ernest Czechowski <ernest.czechowski@gmail.com>
2025-04-14 21:52:58 +0200

    renamed query

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,8 +1,8 @@
 import {graphql} from '@/generated/gql'
 
 export const GET_ALL_USERS_QUERY = graphql(`
-query Query($limit: Int, $offset: Int) {
-  getUsers(limit: $limit, offset: $offset) {
+query Users($limit: Int, $offset: Int) {
+  Users(limit: $limit, offset: $offset) {
     items {
       id
       firstName
